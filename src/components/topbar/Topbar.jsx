fd5bd28eb1033e79f5bb2d7c84ba3a09993e6821
AfifZuhair08 <afifzuhair08@gmail.com>
2022-02-04 01:43:37 +0800

    first UI design

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './topbar.css'
+
+import { Person, Search, Chat, Notifications } from '@mui/icons-material';
+
+export const Topbar = () => {
+    return (
+        <div className='topbarContainer'>
+            <div className='topbarLeft'>
+                <span className="logo">Fasta Social</span>
+            </div>
+            <div className='topbarCenter'>
+                <div className="searchbar">
+                    <Search />
+                    <input type="text" placeholder='Search friends' className="searchInput" />
+                </div>
+            </div>
+            <div className='topbarRight'>
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/alex.jpg" alt="" className="topbarImage" />
+            </div>
+        </div>
+    );
+};
